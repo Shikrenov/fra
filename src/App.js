73f7073bb1a1d5401f87bde8aabb6f14bedb9965
Petar Shikrenov <shikrenov03@abv.bg>
2024-02-15 16:30:03 +0200

    wip check

--- src/App.js
+++ src/App.js
@@ -2,10 +2,13 @@ import './App.css';
 import React from 'react';
 
 const App = () => {
+  const name = 'John';
+  const isNameShowing = false;
   return (
     <div className="App">
-      <h1>Hello World</h1>
+      <h1>Hello, {isNameShowing ? name : 'someone'}</h1>
     </div>
   );
+
 }
 export default App;
