06a33aeb35078fe1991c22100355a9bfc4d5a262
Petar Shikrenov <shikrenov03@abv.bg>
2024-02-20 22:21:11 +0200

    wip exercises

--- src/App.js
+++ src/App.js
@@ -1,15 +1,8 @@
-import './App.css';
 import React from 'react';
-import {useState} from 'react';
-
 const App = () => {
-  const [counter, setCounter] = useState(0); 
   return (
-    <div className="App">
-      <button onClick ={() => alert ('clicked')}>-</button>
-      <h1>{counter}</h1>
-      <button>+</button>
-      </div>
+    <h1>App</h1>
+
   );
 }
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,9 +1,6 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { ReactDOM } from 'react';
 import App from './App';
-import './index.css';
 
-ReactDOM.render(
-  <App />,
-  document.getElementById('root')
-);
+
+ReactDOM.render(<App />, document.getElementById('root'));
