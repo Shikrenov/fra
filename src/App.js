f3c082d7655091e9106d0fc62c9b1abd5b7dbc06
Petar Shikrenov <shikrenov03@abv.bg>
2024-02-21 21:23:54 +0200

    wip exercises

--- src/App.js
+++ src/App.js
@@ -1,8 +1,46 @@
-import React from 'react';
-
+import {useEffect} from 'react';
+ import React from 'react';
+ import './App.css';
+ import SearchIcon from './search.svg';
+const APIUrl = 'http://www.omdbapi.com?apikey=758fccfd';
+const movie1 = {
+  "Title": "Spiderman the Verse",
+  "Year": "2019",
+  "imdbID": "tt12122034",
+  "Type": "series",
+  "Poster": "N/A"
+}
 const App = () => {
+  const searchMovies = async (title) => {
+    const response = await fetch(`${APIUrl}&s=${title}`);
+    const data = await response.json();
+
+    console.log(data.Search);
+  }
+  useEffect (() => {
+    searchMovies ('Spiderman');
+  }, []);
   return (
-    <h1>App</h1>
+    <div className="app">
+      <h1>MovieLand</h1>
+      <div className="search">
+        <input 
+        placeholder="Search for movies"
+        value="Spiderman"
+        onChange={()=>{}}
+        />
+        <img
+        src={SearchIcon}
+        alt="search"
+        onClick={()=>{}}
+        />
+      </div>
+      <div className ="container">
+        <div classname="movie">
+          <p>{movie1.Year}</p>
+        </div>
+      </div>
+    </div>
   );
 }
 export default App;
